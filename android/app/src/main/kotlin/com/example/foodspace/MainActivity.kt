c526719bb9dbab58aee0c2763af2d27329435e71
Teppei Kikuchi <>
2022-04-23 15:29:47 +0700

    update firebase settings

--- android/app/src/main/kotlin/com/example/foodspace/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/foodspace/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.foodspace
+package com.tetsukick.makananan
 
 import io.flutter.embedding.android.FlutterActivity;
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:foodspace/helper/helper_functions.dart';
 import 'package:foodspace/pages/selectcity_page.dart';
 
 import 'pages/onboarding_page.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
